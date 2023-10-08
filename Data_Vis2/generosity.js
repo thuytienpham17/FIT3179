75f5f6bcb215c6d1f6abadb33042c3e5a26bb668
thuytienpham17 <142761054+thuytienpham17@users.noreply.github.com>
2023-10-08 20:56:48 +1100

    Add files via upload

--- Data_Vis2/generosity.js
+++ Data_Vis2/generosity.js
@@ -1,3 +1,3 @@
 var vg_1 = "generosity.json";
-vegaEmbed("#genrosity", vg_1).then(function(result) {
+vegaEmbed("#generosity", vg_1).then(function(result) {
 }).catch(console.error);
---
+++ Data_Vis2/life_expectancy.js
@@ -0,0 +1,3 @@
+var vg_1 = "life_expectancy.json";
+vegaEmbed("#life_expectancy", vg_1).then(function(result) {
+}).catch(console.error);
